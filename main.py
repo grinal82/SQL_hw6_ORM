008477596a317b06f40dbb7ae48b2e06db4f8e44
girnal82 <grinal82@gmail.com>
2022-06-26 13:21:02 +0300

    added query to select publisher by id

--- main.py
+++ main.py
@@ -21,16 +21,28 @@ create_tables(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-with open("fixtures/tests_data.json", "r") as fd:
-    data = json.load(fd)
 
-for record in data:
-    model = {
-        "publisher": Publisher,
-        "shop": Shop,
-        "book": Book,
-        "stock": Stock,
-        "sale": Sale,
-    }[record.get("model")]
-    session.add(model(id=record.get("pk"), **record.get("fields")))
-session.commit()
+def init_data(session):
+    with open("fixtures/tests_data.json", "r") as fd:
+        data = json.load(fd)
+
+    for record in data:
+        model = {
+            "publisher": Publisher,
+            "shop": Shop,
+            "book": Book,
+            "stock": Stock,
+            "sale": Sale,
+        }[record.get("model")]
+        session.add(model(id=record.get("pk"), **record.get("fields")))
+    session.commit()
+
+
+def find_publiher(session):
+    find_publiher = int(input("Введите id издателя: "))
+    result = session.query(Publisher).filter(Publisher.id == find_publiher).one()
+    print(result)
+
+
+init_data(session)
+find_publiher(session)
